52a9f8867d2ea85eb7620e92983dde6bb5dff165
vsflcn <falcoooneee@proton.me>
2024-07-28 18:19:43 +0200

    create a security group to allow SSH and HTTP access

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,3 +10,28 @@ resoure "aws_subnet" "main" {
     vpc_id = aws_vpc.main.id
     cidr_block = "10.0.0.0/24"
 }
+
+resoure "aws_security_group" "main" {
+    vpc_id = aws_vps.main.id
+
+    ingress {
+        from_port   = 22  # allow SSH access
+        to_port     = 22
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    ingress {
+        from_port   = 80  # allow HTTP access
+        to_port     = 80
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    egress {
+        from_port   = 0  # allow all outbound traffic
+        to_port     = 0
+        protocol    = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
